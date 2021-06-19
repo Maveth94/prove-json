0cc785f11fcd807a1830bae2600a76fab404d6e0
Maveth94 <giovannigiuliodori@outlook.com>
2021-06-19 16:23:53 +0200

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  title = 'Angular Example';
+  products: any = [];
+
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    this.httpClient.get('assets/data.json').subscribe(data => {
+      console.log(data);
+      this.products = data;
+    });
+  }
 }
